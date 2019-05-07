bbba5a2c12be81baa13de3da01e078188b107ab0
BgmLover <3150105495@zju.edu.cn>
2019-05-07 15:27:35 +0800

    add run range

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 import config
 
 if __name__ == '__main__':
-    for index in config.max_index:
+    for index in range(config.max_index):
         command = "python3 simulate_one.py " + str(index)
         os.system(command)
